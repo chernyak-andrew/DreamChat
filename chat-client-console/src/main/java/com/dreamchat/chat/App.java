5df1e4d3a6a43f3aac391f5cc2ff78c1ccc5656d
Andrey <chernyak.andrew@gmail.com>
2014-03-11 18:55:49 +0200

    Was added Ali Rapuha lose in MK!!!"

--- chat-client-console/src/main/java/com/dreamchat/chat/App.java
+++ chat-client-console/src/main/java/com/dreamchat/chat/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main(String[] args){
         System.out.println( "Hello World!" );
         System.out.println( "Ali Rapuha!!!" );
+        System.out.println( "Ali Rapuha lose in MK!!!" );
     }
 }
